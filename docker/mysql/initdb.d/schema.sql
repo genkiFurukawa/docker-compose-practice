bf97c0c45d84445429a3a227ac7f9d5922d9fedd
Genki Furukawa <genki.furukawa@gmail.com>
2019-11-18 22:40:52 +0900

    First commit.

---
+++ docker/mysql/initdb.d/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE sample;
+
+USE sample;
+
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(32) NOT NULL,
+    email VARCHAR(32) NOT NULL,
+    PRIMARY KEY (id)
+);
